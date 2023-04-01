898c8f673296c26da32c4c0c7921c2cce9f6f250
Everton Ferreira Lima <Evertonlima0406@hotmail.com>
2023-04-01 00:18:15 -0300

    Continuação no proximo commit

---
+++ src/componentes/ListaSuspensa/index.js
@@ -0,0 +1,12 @@
+import './ListaSuspensa.css';
+
+const ListaSuspensa = (props) => {
+    return (
+        <div>
+            <label>{props.itens}</label>
+            
+        </div>
+    )
+}
+
+export default ListaSuspensa
