c6a88485206c0bc299e08817f76de434fe3eb8e7
Everton Ferreira Lima <Evertonlima0406@hotmail.com>
2023-04-01 21:10:50 -0300

    dei um espaço

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -6,7 +6,7 @@ const ListaSuspensa = (props) => {
             <label>{props.label}</label>
             <select required = {props.obrigatorio}>
                 {props.itens.map(item => {
-                    return <option key={item}>{item}</option>
+                    return <option key={item} > {item}</option>
                     // Para cada item  da lista [props.item] vai retorna um option
                     // com a chave o próprio nome e o valor esta na lista no formulário
                 })}
